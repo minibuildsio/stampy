887a9d7ddac6b48767a1400552abcac253ae9cdc
Howard Paget <9295596+howardpaget@users.noreply.github.com>
2022-03-22 18:38:28 +0000

    Setup integration test

---
+++ src/integrationTest/java/co/uk/howardpaget/stampy/StampyApplicationTest.java
@@ -0,0 +1,15 @@
+package co.uk.howardpaget.stampy;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StampyApplicationTest {
+
+  @Test
+  void dummy() {
+    assertThat(2).isGreaterThan(1);
+  }
+
+}
